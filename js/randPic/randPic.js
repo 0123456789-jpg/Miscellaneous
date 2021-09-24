bd901b932037a93a895195c7a98caf92f8d1dfd2
C191239 <zsz2007314zsz@sina.com>
2021-09-24 23:24:51 +0800

    Fix the path error.

--- js/randPic/randPic.js
+++ js/randPic/randPic.js
@@ -2,19 +2,19 @@ var rand1 = 0;
 var useRand = 0;
 images = new Array;
 images[1] = new Image();
-images[1].src = "pic/1.png";
+images[1].src = "/Miscellaneous/js/randPic/pic/1.png";
 images[2] = new Image();
-images[2].src = "pic/2.png";
+images[2].src = "/Miscellaneous/js/randPic/pic/2.png";
 images[3] = new Image();
-images[3].src = "pic/3.png";
+images[3].src = "/Miscellaneous/js/randPic/pic/3.png";
 images[4] = new Image();
-images[4].src = "pic/4.png";
+images[4].src = "/Miscellaneous/js/randPic/pic/4.png";
 images[5] = new Image();
-images[5].src = "pic/5.png";
+images[5].src = "/Miscellaneous/js/randPic/pic/5.png";
 images[6] = new Image();
-images[6].src = "pic/6.tiff";
+images[6].src = "/Miscellaneous/js/randPic/pic/6.tiff";
 images[7] = new Image();
-images[7].src = "pic/7.tiff";
+images[7].src = "/Miscellaneous/js/randPic/pic/7.tiff";
 function randomPic() {
 var imageNum = images.length - 1;
 do {
